b282c1130b28d30e417464289b1991e70284921b
maxwellc21 <maxiecletus@gmail.com>
2024-09-29 20:43:28 +1000

    DEPLOY WORKFLOW

--- terraform/container-registry.tf
+++ terraform/container-registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "libraryreg" {
-  name                = "libraryreg"
+  name                = "libraryreg9324"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
